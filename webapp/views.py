cebed2a0c7d3dc4c55c0dada05973360047614ac
Yuki <35814354+Miyooki@users.noreply.github.com>
2018-02-17 06:20:36 -0500

    implementsignupplaceholder

--- webapp/views.py
+++ webapp/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+	
+def signup(request):
+	return render(request, "signup.html", {'content':['This is the sign up page!','Sign up placeholder.']} )
