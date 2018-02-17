821797d9f4a9ffe7f2d6274378276139d5868420
Arnav Verma <arnavverma@hotmail.com>
2018-02-17 09:59:18 -0500

    hi

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
 from . import views
-from webapp.models import Post
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 	url(r'^signup/', views.signup, name="sign up"),
-	url(r'^recipe-form/', ListView.as_view(
-		queryset=Post.objects.all().order_by("-date")[:25],
-		template_name="recipe.html")),
-	)
+	url(r'^recipeform/', views.submit, name="submit recipe"),
 ]
