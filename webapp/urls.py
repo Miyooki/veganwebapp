d671e3308181d76d66587657ad105639b591d71c
Arnav Verma <arnavverma@hotmail.com>
2018-02-17 09:34:37 -0500

    t

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
 from . import views
+from webapp.models import Post
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 	url(r'^signup/', views.signup, name="sign up"),
+	url(r'^recipe-form/', ListView.as_view(
+		queryset=Post.objects.all().order_by("-date")[:25],
+		template_name="recipe.html")),
+	)
 ]
